9d09d042a58af50ebcd7fdd6cfb7c713ca522000
Chris Nesbitt-Smith <chris@cns.me.uk>
2019-03-14 16:32:21 +0000

    initial build

---
+++ index.ts
@@ -0,0 +1,79 @@
+const AWS = require("aws-sdk")
+
+const { AWS_IOT_ENDPOINT_HOST } = process.env
+const AWSiot = new AWS.Iot()
+const awsIot = require('aws-iot-device-sdk')
+const _ = require("lodash")
+
+const thingShadows = awsIot.thingShadow({
+  host: AWS_IOT_ENDPOINT_HOST,
+  protocol: 'wss'
+})
+
+interface Thing {
+  name: string
+  type: string
+  attributes: object
+}
+
+interface Subscription {
+  thing_name: String
+  event_handler: Function
+}
+
+interface StateObject {
+  state: Object
+  metadata: Object
+}
+
+class iot {
+  subscriptions: Array<Subscription>
+  constructor() {
+    // thingShadows.on('delta', this.event_handler)
+  }
+
+  async discovered(thing: Thing, event_handler?: Function) {
+    await this.upsert_thing(thing)
+    await this.subscribe_to_thing(thing.name, event_handler)
+  }
+
+  private event_handler(thing_name: String, state_object: StateObject) {
+    _.filter(this.subscriptions, { thing_name: thing_name })
+      .forEach(subscriber => subscriber.event_handler(state_object.state))
+  }
+
+  async upsert_thing(thing: Thing) {
+    let aws_thing = {
+      thingName: thing.name,
+      thingTypeName: thing.type,
+      attributePayload: {
+        attributes: thing.attributes
+      }
+    }
+    try {
+      await AWSiot.updateThing(aws_thing).promise()
+    } catch (error) {
+      await AWSiot.createThing(aws_thing).promise()
+    }
+  }
+
+  subscribe_to_thing(thing_name: string, event_handler?: Function) {
+    thingShadows.register(thing_name)
+    if (event_handler) {
+      this.subscriptions.push({ thing_name: thing_name, event_handler: event_handler })
+    }
+
+  }
+
+  report(thing_name: string, payload: Object) {
+    console.log(`updating ${thing_name}, payload: ${JSON.stringify(payload)}`)
+    return thingShadows.update(thing_name, { state: { reported: payload } })
+  }
+
+  async destroy_thing(thing_name: String) {
+    await awsIot.thingShadow.unregister(thing_name)
+    await AWSiot.deleteThing({ thingName: thing_name }).promise()
+  }
+
+}
+module.exports = iot
---
+++ test/index.test.ts
@@ -0,0 +1,16 @@
+// const rewire = require("rewire")
+// const rules = rewire("../rules")
+const chai = require("chai")
+// const sinon = require("sinon")
+// chai.use(require("sinon-chai"))
+const expect = chai.expect
+
+describe("dummy test", () => {
+  beforeEach(() => {
+    // rules.__set__("functions", [])
+  })
+
+  it("stub test", () =>
+    expect(true)
+  )
+})
